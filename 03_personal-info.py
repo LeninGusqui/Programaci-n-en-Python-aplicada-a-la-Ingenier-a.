d55e3e1686ae3e926c82ece903975529f62d5ba7
LeninGusqui <89555961+LeninGusqui@users.noreply.github.com>
2021-08-26 17:58:41 -0500

    Add files via upload

---
+++ 03_personal-info.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 23 22:43:42 2021
+
+@author: Lenin Gusqui
+"""
+
+nombre= input("Ingrese su nombre: ")
+print("Nombre: "+nombre)
+
+apellido = input("Ingrese su apellido: ")
+print("Apellido: " + apellido)
+
+ciudad = input("Ingrese su ciudad: ")
+print("Ciudad: " + ciudad)
+
+edad = input("Ingrese su edad: ")
+print("Edad: " + edad)
+
+espacio = " "
+
+print("Bienvenido! "+nombre+espacio+apellido+espacio+"Su ciudad es: "+ciudad +espacio+"y su edad es: "+edad)
