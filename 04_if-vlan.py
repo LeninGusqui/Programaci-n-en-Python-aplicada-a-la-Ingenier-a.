4dfa89d34f0b7d6711433c064e789b9990ef0fd8
LeninGusqui <89555961+LeninGusqui@users.noreply.github.com>
2021-08-26 18:06:41 -0500

    Add files via upload

---
+++ 04_if-vlan.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 23 23:09:47 2021
+
+@author: Lenin Gusqui
+"""
+
+nativeVLAN = 100
+dataVLAN = 100
+
+if nativeVLAN == dataVLAN:
+    print("La nativeVLAN y la dataVLAN son las mismas")
+
+else:
+    print("La nativeVLAN y la dataVLAN son diferentes")
